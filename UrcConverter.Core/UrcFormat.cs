8da71b95fb324dc20ee668374c9ce72b3fe36cad
CloudHolic <cloudholic@pm.me>
2026-04-03 18:31:06 +0900

    Add version constant

---
+++ UrcConverter.Core/UrcFormat.cs
@@ -0,0 +1,12 @@
+﻿namespace UrcConverter.Core;
+
+/// <summary>
+///  URC format constants.
+/// </summary>
+public static class UrcFormat
+{
+    /// <summary>
+    /// Current URC format version string.
+    /// </summary>
+    public const string Version = "1.1";
+}
